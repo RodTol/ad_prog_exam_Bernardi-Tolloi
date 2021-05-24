dff9f6fe337ba9a5de54640f7238e4004e5ef41e
giuliabernardi1 <82617812+giuliabernardi1@users.noreply.github.com>
2021-05-24 17:12:44 +0200

    Update bst.hpp

--- include/bst.hpp
+++ include/bst.hpp
@@ -3,9 +3,9 @@
 *
 * Purpouse: create a custom class that represents a
 * binary search tree (bst). This is done by defining a
-* compare operator and a head. The first is the relation 
-* that will decide where a new node will insert, while
-* the second is a unique pointer to the first node of 
+* compare operator and a head. The first one is the relation 
+* that will decide where a new node will be inserted while
+* the second one is an unique pointer to the first node of 
 * the tree. 
 *
 * Date: May, 2021.
@@ -27,15 +27,13 @@ class bst
     OP compare_operator;
 
     /** Find utility function:
-     * this function return a pointer instead of an iterator and
+     * This function returns a pointer instead of an iterator and
      * this feature is very useful in other statements.
      * It works by taking the key and comparing it with each node.
      * If it is equal, obviously the pointer to the node is returned.
-     * If it is greater or less than the key contained in the node,
-     * the pointer will move following the ight or left pointer 
-     * of the first node. In this way the search is speeded up,
-     * removing automaticcaly the nodes that cannot for sure have
-     * equal key */
+     * If it is bigger or smaller than the key contained in the node,
+     * the pointer will move following the right or left pointer 
+     * of the first node. */
     node<attr_type>* _find(const key_type& x) const {
 
         if (!head) {
@@ -66,13 +64,13 @@ class bst
     /** Insert utility function
     * This function is used for both insert and emplace. It
     * will return an iterator to the new node if created, or
-    * to the node with equal key if it already exist.
-    * Firstly it removes the equal case by calling the _find()
-    * function. If it doesn't find it, it will look for the 
+    * to the node with equal key if it already exists.
+    * Firstly, it removes the equal case by calling the _find()
+    * function. If it doesn't find it, it will look for the correct
     * place to create the new node, moving to the right or to
-    * the left. The search will stop when it find a nullptr and then
+    * the left. The search will stop when it finds a nullptr and then
     * to create the new node it will be performed a call to
-    * create_left_child or create_right_child, depending by the case*/
+    * create_left_child or create_right_child, depending on the case.*/
     template <typename O>
     std::pair< iterator, bool> _insert(O&& x)
     {
@@ -121,7 +119,7 @@ public:
     /**Default destructor*/
     ~bst() = default;
 
-    /**Custom constructor, who take a attribute and create a node
+    /**Custom constructor, which takes an attribute and creates a node
      * which will be the first of a bst.*/
     explicit bst (attr_type starting_attr) noexcept:
     head {std::make_unique<node <attr_type> >(std::forward<attr_type>(starting_attr), nullptr)} {}
@@ -157,7 +155,7 @@ public:
     }
 
     /**Begin:
-     * this function will return an iterator to the leftmost node
+     * This function will return an iterator to the leftmost node
      * of the non-const tree.
     */
     iterator begin() noexcept {
@@ -173,7 +171,7 @@ public:
     }
     
     /**Begin:
-     * this function will return a const iterator to the
+     * This function will return a const iterator to the
      * leftmost node of the const tree.
      */
     const_iterator begin() const noexcept {
@@ -189,7 +187,7 @@ public:
     }
 
     /** Cbegin:
-     *  it will always return a const_iterator to the leftmost
+     *  It will always return a const_iterator to the leftmost
      *  node of the tree.*/
     const_iterator cbegin() const noexcept {
         if (!head) {
@@ -204,8 +202,8 @@ public:
     }
 
     /** Tail:
-     * it will return a iterator to the rightmost node of the
-     * non const tree.
+     * It will return an iterator to the rightmost node of the
+     * non-const tree.
     */
     iterator tail() noexcept{
             if (!head)
@@ -222,7 +220,7 @@ public:
     }
 
     /** Tail:
-     * it will return a const_iterator to the rightmost node 
+     * It will return a const_iterator to the rightmost node 
      * of the const tree.
     */
     const_iterator tail() const noexcept{
@@ -240,8 +238,8 @@ public:
     }
 
     /** ctail:
-     * it will return a const_iterator to the rightmost node
-     * of the tree, both for const and non-const tree.
+     * It will return a const_iterator to the rightmost node
+     * of the tree.
     */
     const_iterator ctail() const noexcept{
             if (!head)
@@ -258,7 +256,7 @@ public:
     }
 
     /** End:
-     * it will return an iterator to one-past the last
+     * It will return an iterator to one-past the last
      * element of the tree.
     */
     iterator end() noexcept{
@@ -266,7 +264,7 @@ public:
     }
 
     /** End:
-     * it will return a const iterator to one-past the last
+     * It will return a const iterator to one-past the last
      * element of the const tree.
     */
     const_iterator end() const noexcept{
@@ -274,7 +272,7 @@ public:
     }
 
     /** cend:
-     * it will return a const_iterator to one-past the
+     * It will return a const_iterator to one-past the
      * last element, both for const and non-const tree.
     */
     const_iterator cend() const noexcept{
@@ -282,50 +280,50 @@ public:
     }
 
     /**Find:
-     * search a node with key equal to the input, and if
-     * it succed return a iterator to the node, otherwise,
-     * it will return a iterator to nullptr.
+     * It looks for a node with key equal to the input. If
+     * it succeeds, it returns an iterator to the node, otherwise,
+     * it returns an iterator to nullptr.
     */
     iterator find(const key_type& x) {
         return iterator{_find(x)};
     }
 
     /** Find:
-     * search a node with key equal to the input, and if
-     * it succed return a const_iterator to the node, otherwise,
-     * it will return a const_iterator to nullptr.
+     * It looks for a node with key equal to the input. If
+     * it succeeds, it returns a const_iterator to the node, otherwise,
+     * it returns a const_iterator to nullptr.
      */
     const_iterator find(const key_type& x) const {
         return const_iterator{_find(x)};
     }
 
     /** Insert:
-     * this function tries to create a node with the key 
-     * equal to the input x. It return a pair made by a 
-     * boolean and a iterator. The boolean is false if 
-     * the node with key = x already exist, true if the
-     * node must be created. The iterator point to the node
-     * both if it was just created or it was already there.
-     * This formulation is for l-values
+     * This function tries to create a node with the key 
+     * equal to the input x. It returns a pair made by a 
+     * boolean and an iterator. The boolean is false if 
+     * the node with key = x already exists, true if the
+     * node must be created. The iterator points to the node
+     * both if it was just created or if it was already there.
+     * This formulation is for l-values.
     */
     std::pair<iterator, bool> insert(const attr_type& x){
         return _insert(x);
     }
     /** Insert:
-     * this function tries to create a node with the attribute
-     * equal to the input x. It return a pair made by a 
+     * This function tries to create a node with the attribute
+     * equal to the input x. It returns a pair made by a 
      * boolean and a iterator. The boolean is false if 
-     * the node with key = x already exist, true if the
-     * node must be created. The iterator point to the node
-     * both if it was just created or it was already there.
-     * This formulation is for r-values
+     * the node with key = x already exists, true if the
+     * node must be created. The iterator points to the node
+     * both if it was just created or if it was already there.
+     * This formulation is for r-values.
     */
     std::pair<iterator, bool> insert(attr_type&& x){
         return _insert(std::move(x));
     }
 
     /**Emplace:
-     * this function tries to create one or more element 
+     * This function tries to create one or more element 
      * if there is no element with the same key.
     */
     template< class... Types >
@@ -335,10 +333,10 @@ public:
 
 
     /**subscripting operator:
-     * this function returns a reference to the value of
+     * This function returns a reference to the value of
      * the node with the key equal to the input x,
-     * performing a insertion if that key does not already
-     * exist. This formulation is for l-value
+     * performing an insertion if that key does not already
+     * exist. This formulation is for l-value.
      */
     value_type& operator[](const key_type& x){
         auto f=_find(x);
@@ -351,10 +349,10 @@ public:
      }
 
     /**subscripting operator:
-     * this function returns a reference to the value of
+     * This function returns a reference to the value of
      * the node with the key equal to the input x,
-     * performing a insertion if that key does not already
-     * exist. This formulation is for r-value
+     * performing an insertion if that key does not already
+     * exist. This formulation is for r-value.
      */
     value_type& operator[](key_type&& x){
         auto f=_find(std::move(x));
@@ -367,15 +365,15 @@ public:
      }
     
     /**clear:
-     * this function clear the content of the tree by resetting
-     * the head pointer to nullptr
+     * This function clears the content of the tree by resetting
+     * the head pointer to nullptr.
      */
     void clear() {
         head.reset(nullptr);
     }
 
     /** put-to:
-     * custom overloading of put-to operator that will 
+     * Custom overloading of the put-to operator that will 
      * print the tree following the traversal.
     */
     friend
@@ -388,8 +386,8 @@ public:
     }
 
     /**size:
-     * function that returns the number of nodes inside
-     * the tree
+     * Function that returns the number of nodes inside
+     * the tree.
     */
     std::size_t size() {
         std::size_t _size{0};
@@ -402,10 +400,10 @@ public:
     }
 
     /**balance:
-     * function that will balance the tree, rewriting it
-     * with roughly half of the nodes on the left and
-     * the other half on the right. This first overloading
-     * create a orderd vector "tmp" by taking the attributes of
+     * Function that will balance the tree, rewriting it
+     * with roughly half of the nodes on the left side and
+     * the other half on the right side. This first overloading
+     * creates an orderd vector "tmp" by taking the attributes of
      * the nodes in the traversal order. After that it will
      * call the clear function, erasing the content of the 
      * tree. In the end it will call the second overloding
@@ -429,14 +427,14 @@ public:
     }
 
     /**balance:
-     * this second overloading of the balance function will
-     * take a input vector of attributes and use the insert 
+     * This second overloading of the balance function will
+     * take an input vector of attributes and use the insert 
      * function to create the nodes such that the tree will be
-     * balanced. To achieve so, it will start by inserting the
+     * balanced. To achieve this, it will start by inserting the
      * node in the middle of the vector (or the roundown value
      * if the size of the vector is odd). Than it will
      * repeat the same process for one half of the vector,
-     * inserting an another node, and by iterating this process
+     * inserting an another node. By iterating this process
      * it will create the balanced tree.
     */
     void balance(std::vector<attr_type>& tmp, std::size_t left, std::size_t right) noexcept {
@@ -449,8 +447,8 @@ public:
     }
 
     /**min_sub:
-     * this function return a pointer to the leftmost node
-     * of the subtree of a input node.
+     * This function returns a pointer to the leftmost node
+     * of the subtree of an input node.
     */
     node<attr_type>* min_sub (node<attr_type>* start) {
         while(start -> left) {
@@ -460,11 +458,11 @@ public:
     }    
 
     /**erase:
-     * this function purpouse is to remove a node with key 
-     * equal to the the input (if it exist). The first step is
-     * to check if this node exist. There are 3 case: the node
-     * has no child, has one child (left or right) and has 
-     * both child. For the first case the function simply remove
+     * This function's purpouse is to remove a node with key 
+     * equal to the the input (if it exists). The first step is
+     * to check if this node exists. There are 3 cases: the node
+     * has no child, has one child (left or right) or has 
+     * both children. For the first case the function simply removes
      * the node by resetting the father's pointer and the 
      * objective node's parent pointer. For the second case 
      * it will also connect the father node to the children,
@@ -472,7 +470,7 @@ public:
      * solved by moving the left subtree of the objective node
      * to the leftmost element of the right subtree of the
      * objective node. Thanks to this operation, the node will
-     * have only a right child, and so the problem can be solved
+     * only have a right child, and so the problem can be solved
      * as explained above. 
      */ 
     void erase(const key_type& x) {
@@ -501,7 +499,7 @@ public:
                     }
                 }
             }
-            /*the node has only left child*/
+            /*the node only has aleft child*/
             else if ( (_find(x)->left) &&  !(_find(x)->right) )
             {
                 if ( !(_find(x)->parent) )
@@ -524,7 +522,7 @@ public:
                 }
                 
             }
-            /*the node has only right child*/
+            /*the node only has a right child*/
             else if ( !(_find(x)->left) &&  (_find(x)->right) )
             {
                 if ( !(_find(x)->parent) )
@@ -547,12 +545,12 @@ public:
                 }
                 
             }
-            /*the node has both left and right child*/
+            /*the node has both left and right child
             else if ( (_find(x)->left) &&  (_find(x)->right))
             {
-                /*i move the left subtree of _find(x)* to the left of the
-                leftomost of his right subtree. So, i am again in one of 
-                the case above*/
+                /*I move the left subtree of _find(x)* to the left of the
+                leftomost node of its right subtree. So, I am again in one of 
+                the cases above*/
                 node<attr_type>* min_of_subtree {min_sub(_find(x)->right.get())};
                 _find(x)->left->parent = min_of_subtree;
                 min_of_subtree->left = std::move(_find(x)->left);
